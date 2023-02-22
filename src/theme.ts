df0f15fd40810f30b725c90837c760ed290a41f3
hheeseung <ssigu69@gmail.com>
2023-02-22 17:25:23 +0900

    Design: Theme 색상 지정

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+const darkTheme: DefaultTheme = {
+  bgColor: "#192a56",
+  textColor: "#f5f6fa",
+  accentColor: "#4cd137",
+};
+
+export { darkTheme };
